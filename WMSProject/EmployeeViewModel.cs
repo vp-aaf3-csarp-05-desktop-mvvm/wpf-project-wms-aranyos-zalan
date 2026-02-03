faf87875ac32ffbeb9adf38ad13028c73eceec86
YakataR9 <aranyos.zalan@gmail.com>
2026-02-03 10:35:24 +0100

    A propertyt get-elvehtővé tesszük, hogy az adat megjelenjen

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace WMSProject
+﻿using System.Windows.Media;
+
+namespace WMSProject
 {
     class EmployeeViewModel
     {
-        public string Name = "Nagy Anna";
+        public string Name { get; } = "Nagy Anna";
+        public string Adoszam { get; } = "1234567-09-01";
+
+        public int SzabadnapokSzama { get; } = 30;
+
+
     }
 }
