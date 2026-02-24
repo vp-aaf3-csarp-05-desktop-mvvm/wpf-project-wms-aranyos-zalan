60d60f9c6355c61b8a938eee58663551061f9949
YakataR9 <aranyos.zalan@gmail.com>
2026-02-24 10:16:09 +0100

    Community Toolkit

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -4,11 +4,13 @@ namespace WMSProject
 {
     class EmployeeViewModel
     {
+        private static int VacationDay = 45;
+
         public string Name { get; } = "Nagy Anna";
         public string Adoszam { get; } = "1234567-09-01";
 
-        public int SzabadnapokSzama { get; } = 30;
+        public int SzabadnapokSzama { get; set; } = 8;
 
-
+        public int RemainingDays => VacationDay - SzabadnapokSzama;
     }
 }
